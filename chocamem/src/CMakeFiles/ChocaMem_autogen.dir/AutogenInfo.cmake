052dd7f5d551cda206581fd247d17c9906ed26de
salabeta <enriqueGutierrT@gmail.com>
2019-10-15 17:29:07 +0200

    Maquina de estados con memoria pero sin utilizarla

---
+++ chocamem/src/CMakeFiles/ChocaMem_autogen.dir/AutogenInfo.cmake
@@ -0,0 +1,39 @@
+# Meta
+set(AM_MULTI_CONFIG "SINGLE")
+# Directories and files
+set(AM_CMAKE_BINARY_DIR "/home/salabeta/robocomp/components/beta-robotica-class/chocamem/")
+set(AM_CMAKE_SOURCE_DIR "/home/salabeta/robocomp/components/beta-robotica-class/chocamem/")
+set(AM_CMAKE_CURRENT_SOURCE_DIR "/home/salabeta/robocomp/components/beta-robotica-class/chocamem/src/")
+set(AM_CMAKE_CURRENT_BINARY_DIR "/home/salabeta/robocomp/components/beta-robotica-class/chocamem/src/")
+set(AM_CMAKE_INCLUDE_DIRECTORIES_PROJECT_BEFORE "")
+set(AM_BUILD_DIR "/home/salabeta/robocomp/components/beta-robotica-class/chocamem/src/ChocaMem_autogen")
+set(AM_SOURCES "/home/salabeta/robocomp/classes/qlog/qlog.cpp;/home/salabeta/robocomp/classes/rapplication/rapplication.cpp;/home/salabeta/robocomp/classes/sigwatch/sigwatch.cpp;/home/salabeta/robocomp/components/beta-robotica-class/chocamem/src/commonbehaviorI.cpp;/home/salabeta/robocomp/components/beta-robotica-class/chocamem/src/genericmonitor.cpp;/home/salabeta/robocomp/components/beta-robotica-class/chocamem/src/genericworker.cpp;/home/salabeta/robocomp/components/beta-robotica-class/chocamem/src/main.cpp;/home/salabeta/robocomp/components/beta-robotica-class/chocamem/src/rcismousepickerI.cpp;/home/salabeta/robocomp/components/beta-robotica-class/chocamem/src/specificmonitor.cpp;/home/salabeta/robocomp/components/beta-robotica-class/chocamem/src/specificworker.cpp")
+set(AM_HEADERS "")
+# Qt environment
+set(AM_QT_VERSION_MAJOR "4")
+set(AM_QT_VERSION_MINOR "8")
+set(AM_QT_MOC_EXECUTABLE "/usr/lib/x86_64-linux-gnu/qt4/bin/moc")
+set(AM_QT_UIC_EXECUTABLE "/usr/lib/x86_64-linux-gnu/qt4/bin/uic")
+set(AM_QT_RCC_EXECUTABLE )
+# MOC settings
+set(AM_MOC_SKIP "/home/salabeta/robocomp/components/beta-robotica-class/chocamem/src/ChocaMem_autogen/mocs_compilation.cpp;/home/salabeta/robocomp/components/beta-robotica-class/chocamem/src/CommonBehavior.cpp;/home/salabeta/robocomp/components/beta-robotica-class/chocamem/src/DifferentialRobot.cpp;/home/salabeta/robocomp/components/beta-robotica-class/chocamem/src/GenericBase.cpp;/home/salabeta/robocomp/components/beta-robotica-class/chocamem/src/Laser.cpp;/home/salabeta/robocomp/components/beta-robotica-class/chocamem/src/RCISMousePicker.cpp")
+set(AM_MOC_DEFINITIONS "QT_CORE_LIB;QT_GUI_LIB;QT_NO_DEBUG;QT_OPENGL_LIB;QT_SQL_LIB;QT_XML_LIB")
+set(AM_MOC_INCLUDES "/home/salabeta/robocomp/components/beta-robotica-class/chocamem/src/ChocaMem_autogen/include;/home/salabeta/robocomp/components/beta-robotica-class/chocamem/src;/home/salabeta/robocomp/components/beta-robotica-class/chocamem/src/.;/opt/robocomp/include;/classes;/home/salabeta/robocomp/components/beta-robotica-class/chocamem;/usr/include/qt4;/usr/include/qt4/QtOpenGL;/usr/include/qt4/QtGui;/usr/include/qt4/QtXml;/usr/include/qt4/QtSql;/usr/include/qt4/QtCore;/home/salabeta/robocomp/classes;/home/salabeta/robocomp/libs;/home/salabeta/robocomp/interfaces;/include")
+set(AM_MOC_OPTIONS "")
+set(AM_MOC_RELAXED_MODE "FALSE")
+set(AM_MOC_MACRO_NAMES "Q_OBJECT;Q_GADGET;Q_NAMESPACE")
+set(AM_MOC_DEPEND_FILTERS "")
+set(AM_MOC_PREDEFS_CMD )
+# UIC settings
+set(AM_UIC_SKIP "/home/salabeta/robocomp/components/beta-robotica-class/chocamem/src/ChocaMem_autogen/mocs_compilation.cpp;/home/salabeta/robocomp/components/beta-robotica-class/chocamem/src/CommonBehavior.cpp;/home/salabeta/robocomp/components/beta-robotica-class/chocamem/src/DifferentialRobot.cpp;/home/salabeta/robocomp/components/beta-robotica-class/chocamem/src/GenericBase.cpp;/home/salabeta/robocomp/components/beta-robotica-class/chocamem/src/Laser.cpp;/home/salabeta/robocomp/components/beta-robotica-class/chocamem/src/RCISMousePicker.cpp")
+set(AM_UIC_TARGET_OPTIONS "")
+set(AM_UIC_OPTIONS_FILES "")
+set(AM_UIC_OPTIONS_OPTIONS "")
+set(AM_UIC_SEARCH_PATHS "")
+# RCC settings
+set(AM_RCC_SOURCES )
+set(AM_RCC_BUILDS )
+set(AM_RCC_OPTIONS )
+set(AM_RCC_INPUTS )
+# Configurations options
+set(AM_CONFIG_SUFFIX_ "_")
---
+++ chocamem/src/specificworker.cpp
@@ -0,0 +1,180 @@
+/*
+ *    Copyright (C)2019 by YOUR NAME HERE
+ *
+ *    This file is part of RoboComp
+ *
+ *    RoboComp is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    RoboComp is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "specificworker.h"
+
+/**
+* \brief Default constructor
+*/
+SpecificWorker::SpecificWorker(MapPrx& mprx) : GenericWorker(mprx)
+{}
+
+/**
+* \brief Default destructor
+*/
+SpecificWorker::~SpecificWorker()
+{
+	std::cout << "Destroying SpecificWorker" << std::endl;
+}
+
+bool SpecificWorker::setParams(RoboCompCommonBehavior::ParameterList params)
+{
+	
+	RoboCompCommonBehavior::Parameter par = params.at("InnerModelPath");
+	innerModel = std::make_shared<InnerModel>(par.value);
+	xmin = std::stoi(params.at("xmin").value);
+	xmax = std::stoi(params.at("xmax").value);
+	ymin = std::stoi(params.at("ymin").value);
+	ymax = std::stoi(params.at("ymax").value);
+	tilesize = std::stoi(params.at("tilesize").value);
+
+	// Scene
+ 	scene.setSceneRect(xmin, ymin, fabs(xmin)+fabs(xmax), fabs(ymin)+fabs(ymax));
+ 	view.setScene(&scene);
+ 	view.scale(1, -1);
+ 	view.setParent(scrollArea);
+ 	view.fitInView(scene.sceneRect(), Qt::KeepAspectRatio );
+	grid.initialize( TDim{ tilesize, xmin, xmax, ymin, ymax}, TCell{true, false, nullptr} );
+
+	qDebug() << "Grid initialize ok";
+
+	for(auto &[key, value] : grid)
+ 	{
+	 	value.rect = scene.addRect(-tilesize/2,-tilesize/2, 100,100, QPen(Qt::NoPen));			
+		value.rect->setPos(key.x,key.z);
+	}
+
+ 	robot = scene.addRect(QRectF(-200, -200, 400, 400), QPen(), QBrush(Qt::blue));
+ 	noserobot = new QGraphicsEllipseItem(-50,100, 100,100, robot);
+ 	noserobot->setBrush(Qt::magenta);
+
+	view.show();
+	return true;
+}
+
+void SpecificWorker::initialize(int period)
+{
+	std::cout << "Initialize worker" << std::endl;
+
+	this->Period = period;
+	timer.start(Period);
+	qDebug() << "End initialize";
+
+}
+
+void SpecificWorker::compute()
+{
+	readRobotState();
+
+	/// AQUI LA MAQUINA DE ESTADOS
+	const float threshold = 200; // millimeters
+    float rot = 0.8;  // rads per second
+	int currentState;
+    try
+    {
+    	// read laser data 
+        RoboCompLaser::TLaserData ldata = laser_proxy->getLaserData(); 
+		//sort laser data from small to large distances using a lambda function.
+        std::sort( ldata.begin(), ldata.end(), [](RoboCompLaser::TData a, RoboCompLaser::TData b){ return     a.dist < b.dist; });  
+        if( ldata.front().dist < threshold)
+			currentState = 0;
+		else{
+			if( ldata.front().dist < 500)
+				currentState = 1;
+		}
+		switch(currentState){
+			case 0:
+				std::cout << currentState << std::endl;
+				std::cout << ldata.front().dist << std::endl;
+ 				differentialrobot_proxy->setSpeedBase(5, rot);
+				usleep(rand()%(1500000-100000 + 1) + 100000);  // random wait between 1.5s and 0.1sec
+				currentState = 1;
+				break;
+			case 1:
+				std::cout << currentState << std::endl;
+				differentialrobot_proxy->setSpeedBase(800, 0);
+				currentState = 2;
+				break;
+			default:
+				std::cout << currentState << std::endl;
+				differentialrobot_proxy->setSpeedBase(1000, 0);
+				break;
+		}
+
+    }
+    catch(const Ice::Exception &ex)
+    {
+        std::cout << ex << std::endl;
+    }
+}
+	
+
+void SpecificWorker::readRobotState()
+{
+	try
+	{
+		differentialrobot_proxy->getBaseState(bState);
+		innerModel->updateTransformValues("base", bState.x, 0, bState.z, 0, bState.alpha, 0);
+		RoboCompLaser::TLaserData ldata = laser_proxy->getLaserData();
+		
+		//draw robot
+		robot->setPos(bState.x, bState.z);
+		robot->setRotation(-180.*bState.alpha/M_PI);
+
+		//update  occupied cells
+		updateOccupiedCells(bState, ldata);
+	}
+	catch(const Ice::Exception &e)
+	{
+		std::cout << "Error reading from Laser" << e << std::endl;
+	}
+	//Resize world widget if necessary, and render the world
+	if (view.size() != scrollArea->size())
+	 		view.setFixedSize(scrollArea->width(), scrollArea->height());
+	
+}
+
+void SpecificWorker::updateOccupiedCells(const RoboCompGenericBase::TBaseState &bState, const RoboCompLaser::TLaserData &ldata)
+{
+	InnerModelLaser *n = innerModel->getNode<InnerModelLaser>(QString("laser"));
+	for(auto l: ldata)
+	{
+		auto r = n->laserTo(QString("world"), l.dist, l.angle);	// r is in world reference system
+		// we set the cell corresponding to r as occupied 
+		auto [valid, cell] = grid.getCell(r.x(), r.z()); 
+		if(valid)
+		{
+			cell.free = false;
+			cell.rect->setBrush(Qt::darkRed);
+		}
+	}
+}
+
+
+///////////////////////////////////////////////////////////////////77
+////  SUBSCRIPTION && LIKE
+/////////////////////////////////////////////////////////////////////
+
+void SpecificWorker::RCISMousePicker_setPick(const Pick &myPick)
+{
+//subscribesToCODE
+
+}
+
+
+
